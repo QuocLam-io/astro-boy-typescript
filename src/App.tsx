b4fa1f01ce8b6fe745ff88303dc7afa59e7dff3f
frostycakes <lamquoc.24@gmail.com>
2023-01-12 11:49:32 -0500

    components skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.scss";
+import Navbar from "./components/Navbar";
+import Keyboard from "./components/Keyboard";
+import StartPage from "./components/StartPage";
+import WordDisplay from "./components/WordDisplay";
+import ProgressDisplay from "./components/ProgressDisplay";
+
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <StartPage />
+      <Navbar />
+      <WordDisplay />
+      <ProgressDisplay />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const keyboard = () => {
+
+  const topKeys = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p']
+  const middleKeys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
+  const bottomKeys = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+
+  return (
+    <div>keyboard</div>
+  )
+}
+
+export default keyboard
