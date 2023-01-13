b4497a5f21d66bead052fefadaaaff6b5b90a918
frostycakes <lamquoc.24@gmail.com>
2023-01-13 12:51:13 -0500

    WordDisplay Css

--- src/components/WordDisplay.tsx
+++ src/components/WordDisplay.tsx
@@ -5,21 +5,55 @@ type WordDisplayProps = {
 };
 
 const WordDisplay = ({ randomWord }: WordDisplayProps) => {
- const guessedLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+  const guessedLetters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
 
   return (
     <div className="WordDisplay">
+      <p>GUESS THE WORD BELOW</p>
       <div className="word">
         {randomWord.split("").map((letter, index) => {
-          return <span
-          style={{borderBottom: "1px solid white"}}
-          > <span
-          style={{
-
-            visibility: guessedLetters.includes(letter) ? "visible" : "hidden",
-          }}
-          >{letter}</span> </span>;
+          return (
+            <div className="letter-box"
+            key={index}>
+              {" "}
+              <span className="letter"
+                style={{
+                  visibility: guessedLetters.includes(letter)
+                    ? "visible"
+                    : "hidden",
+                }}
+              >
+                {letter}
+              </span>
+            </div>
+          );
         })}
       </div>
     </div>
