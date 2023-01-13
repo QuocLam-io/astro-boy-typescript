313647aaf7ddc5ae466f720a71090c38c16ab30e
frostycakes <lamquoc.24@gmail.com>
2023-01-13 13:12:07 -0500

    setGuessedLetters KeyboardProps

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-type KeyboardProps = {};
+type KeyboardProps = {
+  setGuessedLetters: React.Dispatch<React.SetStateAction<string[]>>;
+};
 
-const Keyboard = ({}: KeyboardProps) => {
+const Keyboard = ({setGuessedLetters}: KeyboardProps) => {
   const topKeys: string[] = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
   const middleKeys: string[] = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
   const bottomKeys: string[] = ["z", "x", "c", "v", "b", "n", "m"];
