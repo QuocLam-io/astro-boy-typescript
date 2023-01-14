0002cdafab660bfe3bab97042765ba5f2dbf915e
frostycakes <lamquoc.24@gmail.com>
2023-01-14 13:53:10 -0500

    addGuessedLetter comment

--- src/App.tsx
+++ src/App.tsx
@@ -51,6 +51,7 @@ const App: React.FC = () => {
 
   //Event Listeners for physical and virtual Keyboard
   const addGuessedLetter = useCallback(
+    //unables adding letter
     (letter: string) => {
       if (guessedLetters.includes(letter) || isLoser || isWinner) return;
 
