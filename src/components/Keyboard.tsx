8aad60a3a5b897aaf6b2f2b4ac74214b23992161
frostycakes <lamquoc.24@gmail.com>
2023-01-14 13:23:25 -0500

    key active and inactive logic

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -6,23 +6,42 @@ type KeyboardProps = {
   incorrectLetters: string[];
 };
 
-const Keyboard = ({ addGuessedLetter, correctLetters, incorrectLetters }: KeyboardProps) => {
+const Keyboard = ({
+  addGuessedLetter,
+  correctLetters,
+  incorrectLetters,
+}: KeyboardProps) => {
   const topKeys: string[] = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
   const middleKeys: string[] = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
   const bottomKeys: string[] = ["z", "x", "c", "v", "b", "n", "m"];
 
   const topRow = topKeys.map((letter) => (
-    <button onClick={() => addGuessedLetter(letter)} className="key">
+    <button
+      onClick={() => addGuessedLetter(letter)}
+      className={`key ${correctLetters.includes(letter) && "correct"} ${
+        incorrectLetters.includes(letter) && "incorrect"
+      }`}
+    >
       {letter}
     </button>
   ));
   const middleRow = middleKeys.map((letter) => (
-    <button onClick={() => addGuessedLetter(letter)} className="key">
+    <button
+      onClick={() => addGuessedLetter(letter)}
+      className={`key ${correctLetters.includes(letter) && "correct"} ${
+        incorrectLetters.includes(letter) && "incorrect"
+      }`}
+    >
       {letter}
     </button>
   ));
   const bottomRow = bottomKeys.map((letter) => (
-    <button onClick={() => addGuessedLetter(letter)} className="key">
+    <button
+      onClick={() => addGuessedLetter(letter)}
+      className={`key ${correctLetters.includes(letter) && "correct"} ${
+        incorrectLetters.includes(letter) && "incorrect"
+      }`}
+    >
       {letter}
     </button>
   ));
