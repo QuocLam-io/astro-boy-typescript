772861974b01a0114ab340f343165693f9235598
frostycakes <lamquoc.24@gmail.com>
2023-01-16 14:40:56 -0500

    How to play modal close function

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import {motion} from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
 interface HowToPlayProps {
-  setHowToPlay: React.Dispatch<React.SetStateAction<boolean>>
+  setHowToPlay: React.Dispatch<React.SetStateAction<boolean>>;
+  howToPlay: boolean;
 }
 
-const HowToPlay = ({setHowToPlay}:HowToPlayProps) => {
+const HowToPlay = ({ setHowToPlay, howToPlay }: HowToPlayProps) => {
   return (
-    <motion.div className='HowToPlay'
-    initial={{opacity: 0}}
-    animate={{opacity: 1}}
-    exit={{opacity: 0}}
+    <motion.div
+      className="HowToPlay"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
-
+      <div id="instructions">
+        <div className="instructions-header">
+          <h2>HOW TO PLAY</h2>
+          <img
+            onClick={() => {
+              setHowToPlay(!howToPlay);
+            }}
+            src="../../public/imgs/closeVector.svg"
+            alt="close button"
+          />
+        </div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis amet
+        beatae repellat nostrum voluptatibus nemo ut vel natus porro odio quam,
+        voluptates delectus dolore excepturi at provident facere fuga.
+      </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default HowToPlay
+export default HowToPlay;
