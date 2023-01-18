103d8214fe50cd0c8b6e4e70d605b11e4b218f84
frostycakes <lamquoc.24@gmail.com>
2023-01-18 16:04:58 -0500

    StartPage html

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -5,8 +5,35 @@ interface StartPageProps {
   setStartGame: (startGame: boolean) => void;
 }
 
-const StartPage: React.FC<StartPageProps> = ({ getRandomWord, setStartGame }) => {
-  return <div className="StartPage"></div>;
+const StartPage: React.FC<StartPageProps> = ({
+  getRandomWord,
+  setStartGame,
+}) => {
+  const title = "ASTROBOY";
+
+  return (
+    <div className="StartPage">
+      <img src="./imgs/planet.svg" alt="cartoon planet" />
+      <div className="word">
+        {title.split("").map((letter, index) => {
+          return (
+            <div className="letter-box" key={index}>
+              {" "}
+              <span className="letter">{letter}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div
+        onClick={() => {
+          getRandomWord();
+        }}
+        className="new-game"
+      >
+        START MISSION
+      </div>
+    </div>
+  );
 };
 
 export default StartPage;
