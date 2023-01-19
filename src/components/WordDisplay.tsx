b52404ad41b17f58105ce163b7bfe58a124199bb
frostycakes <lamquoc.24@gmail.com>
2023-01-19 11:16:22 -0500

    reveal props typo

--- src/components/WordDisplay.tsx
+++ src/components/WordDisplay.tsx
@@ -3,23 +3,26 @@ import React from "react";
 type WordDisplayProps = {
   randomWord: string;
   guessedLetters: string[];
-  reveal?: boolean;
+  revealWord?: boolean;
 };
 
-const WordDisplay = ({ randomWord, guessedLetters, reveal }: WordDisplayProps) => {
-
+const WordDisplay = ({
+  randomWord,
+  guessedLetters,
+  revealWord = false,
+}: WordDisplayProps) => {
   return (
     <div className="WordDisplay">
       <p>GUESS THE WORD BELOW</p>
       <div className="word">
         {randomWord.split("").map((letter, index) => {
           return (
-            <div className="letter-box"
-            key={index}>
+            <div className="letter-box" key={index}>
               {" "}
-              <span className="letter"
+              <span
+                className="letter"
                 style={{
-                  visibility: guessedLetters.includes(letter)
+                  visibility: guessedLetters.includes(letter) || revealWord
                     ? "visible"
                     : "hidden",
                 }}
